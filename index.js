864058514b5d8f8c629ac8e64ad6d594bfc95789
jhonatan soto <jhonrocker2012@gmail.com>
2021-07-01 22:21:03 -0400

    exercise 3.22

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 require('dotenv').config()
 const express = require('express')
-const app = express();
+const app = express()
 const PORT = process.env.PORT || 3001
 const morgan = require('morgan')
 const cors = require('cors')
-const Contact = require('./models/contact');
-const { NativeError } = require('mongoose');
+const Contact = require('./models/contact')
+
 // const contact = require('./models/contact');
-let persons = [];
+let persons = []
 app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('content',(req) => {
-    return req.method === 'POST' 
+  return req.method === 'POST' 
     ? JSON.stringify(req.body)
     : null 
 
 })
 
 app.use(morgan((tokens, req, res) => {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens.content(req, res)
-    ].join(' ')
-  }))
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.content(req, res)
+  ].join(' ')
+}))
 
 morgan.token('body', function(req, res, param) {
-    return req.body;
-});
+  return req.body
+})
 app.use(cors())
 
 
@@ -66,128 +66,128 @@ app.use(cors())
 
 
 app.get('/api/persons',(request,response) => {
-    Contact.find({}).then( contacts => {
+  Contact.find({}).then( contacts => {
         
         
-        persons = contacts
-        console.log(persons)
-        response.json(contacts)
+    persons = contacts
+    console.log(persons)
+    response.json(contacts)
         
-    })
+  })
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
     
-    if(error.name === 'Validation failed'){
-        return response.status(400).send({error : `${error}`})
-    }
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if(error.name === 'ValidationError') {
-      return response.status(400).send({error : `${error}`})
-    }
+  if(error.name === 'Validation failed'){
+    return response.status(400).send({error : `${error}`})
+  }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+    return response.status(400).send({error : `${error}`})
+  }
     
   
-    next(error)
-  }
+  next(error)
+}
 
 app.get('/api/persons/:id',(request,response,next) => {
-    Contact.findById(request.params.id)
+  Contact.findById(request.params.id)
     .then(contact => {
-        if(contact){
-            response.json(contact)
-        } else {
-            response.status(404).end()
-        }
+      if(contact){
+        response.json(contact)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
 app.post('/api/persons/',(request,response,next) => {
-    const {name, number} = request.body
-    console.log(number)
-    const contact = new Contact({
-        name:name,
-        number:number.toString()
+  const {name, number} = request.body
+  console.log(number)
+  const contact = new Contact({
+    name:name,
+    number:number.toString()
         
+  })
+  if(!name || !number){
+    return response.status(400).json({
+      error:'error you need to add a name or a number'
     })
-    if(!name || !number){
-        return response.status(400).json({
-            error:`error you need to add a name or a number`
-        })
-    }
-          contact
-          .save()
-          .then(savedContact => savedContact.toJSON())
-          .then(savedAndFormatted =>{
-              response.json(savedAndFormatted)
-          })
-          .catch(error => {
-            next(error)
-          })
+  }
+  contact
+    .save()
+    .then(savedContact => savedContact.toJSON())
+    .then(savedAndFormatted =>{
+      response.json(savedAndFormatted)
+    })
+    .catch(error => {
+      next(error)
+    })
 
 
-        //   .then(savedContact => {
-        //     if(savedContact){
-        //         response.json(savedContact)
-        //     }else{
-        //         response.status(404).end()
-        //     } 
-        // })
+  //   .then(savedContact => {
+  //     if(savedContact){
+  //         response.json(savedContact)
+  //     }else{
+  //         response.status(404).end()
+  //     } 
+  // })
 
         
-    })
+})
 
 app.delete('/api/persons/:id',(request,response) => {
-    const id = request.params.id
+  const id = request.params.id
     
-    Contact.findByIdAndRemove(id)
+  Contact.findByIdAndRemove(id)
     .then(result => {
-        response.status(204).json({
-            msg:`la persona fue eliminada`
-        })
+      response.status(204).json({
+        msg:'la persona fue eliminada'
+      })
     })
 })
 
 app.get('/info',(request,response)=> {
-    Contact.find({}).then( contacts => {
+  Contact.find({}).then( contacts => {
         
         
-      const count = contacts.length
-      response.send(`
+    const count = contacts.length
+    response.send(`
     <h3>Phonebook has info for ${count} people </h3>
     <p>${new Date()}</p>
     `)
         
-    })
+  })
     
    
     
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const {name, number} = request.body
-    const contact = {
-      name:name,
-      number: number
-    }
-    console.log(number)         
-    Contact.findByIdAndUpdate(request.params.id, contact, { new: true, runValidators: true,context: 'query'},)
-      .then(updatedNote => {
-        console.log(updatedNote)
-        response.json(updatedNote)
-      })
-      .catch(error => next(error))
-  })
+  const {name, number} = request.body
+  const contact = {
+    name:name,
+    number: number
+  }
+  console.log(number)         
+  Contact.findByIdAndUpdate(request.params.id, contact, { new: true, runValidators: true,context: 'query'},)
+    .then(updatedNote => {
+      console.log(updatedNote)
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
 app.listen(PORT,() => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
-var uniqueValidator = require('mongoose-unique-validator');
+var uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 console.log('connection to ',url)
@@ -8,25 +8,25 @@ console.log('connection to ',url)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const noteSchema = new mongoose.Schema({
-    name: {
-      type:String,
-      required:true,
-      unique:true,
-      minLength:3
-    },
-    number: {
-      type:String,
-      required:true,
-      minLength:8
-    }
+  name: {
+    type:String,
+    required:true,
+    unique:true,
+    minLength:3
+  },
+  number: {
+    type:String,
+    required:true,
+    minLength:8
+  }
 })
 noteSchema.plugin(uniqueValidator)
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 
